140f8a46b9dd5144a8bcaa2b51a452905c780df3
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-01-24 07:29:22 +0300

    Post request and response codes

--- src/main/java/org/dilaraseker/Main.java
+++ src/main/java/org/dilaraseker/Main.java
@@ -1,17 +1,94 @@
 package org.dilaraseker;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.OutputStream;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        sendGetRequest();
+        sendPostRequest();
+    }
+
+    private static void sendGetRequest() {
+        try {
+            String apiUrl = "https://jsonplaceholder.typicode.com/users/7";
+
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("GET Request Sent");
+            System.out.println("Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                System.out.println("GET request was successful (200 OK).");
+            } else if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
+                System.out.println("Requested resource not found for GET (404 Not Found).");
+            } else {
+                System.out.println("Unknown HTTP status for GET: " + responseCode);
+            }
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            System.out.println("GET Response: " + response.toString());
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    private static void sendPostRequest() {
+        try {
+            String apiUrl = "https://jsonplaceholder.typicode.com/users/";
+                URL url = new URL(apiUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+
+                // POST data
+                String postData = "{ \"name\": \"Deneme User\", \"username\": \"denemeuser\", \"email\": \"deneme@gmail.com\" }";
+                connection.setDoOutput(true);
+
+                // Write data
+                try (OutputStream os = connection.getOutputStream()) {
+                    byte[] input = postData.getBytes("utf-8");
+                    os.write(input, 0, input.length);
+                }
+
+                int responseCode = connection.getResponseCode();
+                System.out.println("POST Request Sent");
+                System.out.println("Response Code: " + responseCode);
+
+                if (responseCode == HttpURLConnection.HTTP_CREATED) {
+                    System.out.println("POST request was successful (201 Created).");
+                } else if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
+                    System.out.println("Requested resource not found for POST (404 Not Found).");
+                }else {
+                    System.out.println("Unknown HTTP status for POST: " + responseCode);
+                }
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                System.out.println("POST Response: " + response.toString());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
     }
 }
